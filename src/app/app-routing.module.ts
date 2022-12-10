ac65710381e422099a58a5606584312a5e2f4803
Kareemelshekh11 <karemelsheah1998@gmail.com>
2022-12-10 19:17:40 +0200

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GenresComponent } from './genres/genres.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvComponent } from './tv/tv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component:HomeComponent},
+  {path:'movies' , component:MoviesComponent},
+  {path:'tv' , component:TvComponent},
+  {path:'genres' , component:GenresComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SidebarModule } from 'primeng/sidebar';
+import { CarouselModule } from 'primeng/carousel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvComponent } from './tv/tv.component';
+import { GenresComponent } from './genres/genres.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MoviesComponent,
+    TvComponent,
+    GenresComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SidebarModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
